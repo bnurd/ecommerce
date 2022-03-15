63c029cb5ff625c94148d2ed08f0263358420c8a
teavun <tesadumuefkar54@gmail.com>
2022-03-15 16:42:05 +0300

    product files models prepared

---
+++ Models/File.cs
@@ -0,0 +1,11 @@
+﻿namespace ecommerce.Models;
+
+public class File : IEntity
+{
+    public int Id { get; set; }
+    public string Path { get; set; }
+    public DateTime Generated { get; set; }
+    public DateTime Modified { get; set; }
+
+    public Product Product { get; set; }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,9 +2,12 @@ namespace ecommerce.Models;
 
 public class Product : IEntity
 {
-
     public int Id { get; set; }
     public String Title { get; set; }
+    public String Detail { get; set; }
+    public String ShipDetail { get; set; }
+
+    public int Count { get; set; }
 
     public double Price { get; set; }
     public Currency Currency { get; set; }
@@ -13,5 +16,6 @@ public class Product : IEntity
     public Category Category { get; set; }
 
     public List<Order> Orders { get; set; } // many to many
+    public List<File> Files { get; set; } 
 
 }
